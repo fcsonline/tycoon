9174fa9678b43b87ae8ba77bde7816eb709c8829
Saimon Moore <saimonmoore@gmail.com>
2016-10-23 16:54:38 +0200

    Initial import

---
+++ contracts/VesselState.sol
@@ -0,0 +1,14 @@
+contract VesselState {
+  uint public indentifier;
+  uint public indentifier_type;
+  uint public updated_at;
+  uint public latitude;
+  uint public longitude;
+  uint public altitude;
+  uint public velocity;
+  uint public heading;
+
+  function VesselState() {
+    // constructor
+  }
+}
