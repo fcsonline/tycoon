1c77aa8d9b726d226bea63e1060046203d18efc6
Saimon Moore <saimonmoore@gmail.com>
2016-10-23 18:59:21 +0200

    broken syntax

--- contracts/VesselState.sol
+++ contracts/VesselState.sol
@@ -3,50 +3,50 @@ pragma solidity ^0.4.2;
 import "VesselStateLib.sol";
 
 contract VesselState {
-	struct State {
-		address verified_by;
-    string  vessel_type;
-		uint		indentifier;
-		string	indentifier_type;
-		uint		updated_at;
-		uint		latitude;
-		uint		longitude;
-		uint		altitude;
-		uint		velocity;
-		uint		heading;
-	}
+    struct State {
+        address verified_by;
+        string  vessel_type;
+        uint    indentifier;
+        string	indentifier_type;
+        uint    updated_at;
+        uint	latitude;
+        uint	longitude;
+        uint	altitude;
+        uint	velocity;
+        uint	heading;
+    }
 
-  mapping (string => State) states;
+    mapping (string => State) states;
 
-  function VesselState() {
-    // setup transaction validators/origins
-  }
+    function VesselState() {
+        // setup transaction validators/origins
+    }
 
-  function newState(
-    address verified_by,
-    string  vessel_type,
-    uint    identifier,
-    string  identifier_type,
-    uint    latitude,
-    uint    longitude,
-    uint    altitude,
-    uint    velocity,
-    uint    heading,
-    uint    updated_at
-  ) {
-    
-    string memory global_identifier = VesselStateLib.calculateStateIdentifier(vessel_type, identifier, identifier_type);
-    states[global_identifier] = State(
-			verified_by,
-      vessel_type,
-			identifier,
-			identifier_type,
-			latitude,
-			longitude,
-			altitude,
-			velocity,
-			heading,
-			updated_at
-    );
-  }
+    function newState(
+        address verified_by,
+        string  vessel_type,
+        uint    identifier,
+        string  identifier_type,
+        uint    latitude,
+        uint    longitude,
+        uint    altitude,
+        uint    velocity,
+        uint    heading,
+        uint    updated_at
+    ) {
+
+        string global_identifier = VesselStateLib.calculateStateIdentifier(vessel_type, identifier, identifier_type);
+        states[global_identifier] = State(
+            verified_by,
+            vessel_type,
+            identifier,
+            identifier_type,
+            latitude,
+            longitude,
+            altitude,
+            velocity,
+            heading,
+            updated_at
+        );
+    }
 }
